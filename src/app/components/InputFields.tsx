d456ddd64dedbe350d61008399ae0c40246cd5eb
AbazuzyC <abufadelabas20@gmail.com>
2024-10-12 12:01:39 +0700

    It can press enter now

--- src/app/components/InputFields.tsx
+++ src/app/components/InputFields.tsx
@@ -1,23 +1,31 @@
-import React from "react";
-interface Props{
-  todo:string;
+import React, { useRef } from "react";
+
+interface Props {
+  todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
-
-
+  handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputFields: React.FC<Props> = ({todo, setTodo}) => {
+const InputFields: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="flex w-[95%] items-center relative">
+    <form
+      className="input flex w-[95%] items-center relative"
+      onSubmit={(e) => {
+        handleAdd(e);          // Handles the form submission
+        setTodo('');           // Clears the input after submission
+        inputRef.current?.blur(); // Blurs the input to reset focus (fade effect)
+      }}
+    >
       <input
+        ref={inputRef}
         type="text"
         placeholder="Type what you wanna type"
         className="w-full rounded-full p-5 text-xl border-none transition duration-200 shadow-inner 
         focus:shadow-[0_0_10px_1000px_rgba(0,0,0,0.1)] focus:outline-none"
         value={todo}
-        onChange={
-          (e)=>setTodo(e.target.value)
-        }
+        onChange={(e) => setTodo(e.target.value)}
       />
       <button
         className="absolute w-[50px] h-[50px] m-3 rounded-full right-0 border-none 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,34 @@
 "use client"
 
 import InputFields from "./components/InputFields";
+import { Todo } from "./components/model";
 import "./globals.css";
 import { useState } from "react";
 
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+  
+  const handleAdd = (e:React.FormEvent) => {
+    e.preventDefault();
 
-  console.log(todo)
+    if(todo){
+      setTodos([...todos,{id:Date.now(),todo,isDone:false}])
+      setTodo("");
+    }
+  };
+
+  console.log(todos);
+
 
   return (
     <div className="w-lvw h-lvh flex flex-col items-center bg-blue-300">
       <span className="font-serif uppercase text-4xl mt-8 text-black z-10 text-center mb-3" >to-do-list</span>
-      <InputFields todo={todo} setTodo={setTodo}/>
+      <InputFields todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      {todos.map((t)=>(
+        <li>{t.todo}</li>
+      ))}
     </div>
   );
 }
