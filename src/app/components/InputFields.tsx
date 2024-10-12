3a0cf260c4fd9a2921f76738191694616ce07bcd
AbazuzyC <abufadelabas20@gmail.com>
2024-10-12 10:08:09 +0700

    A little bit of changes

--- src/app/components/InputFields.tsx
+++ src/app/components/InputFields.tsx
@@ -1,6 +1,12 @@
-import React from "react"
+import React from "react";
+interface Props{
+  todo:string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
 
-const InputFields = () => {
+
+}
+
+const InputFields: React.FC<Props> = ({todo, setTodo}) => {
   return (
     <form className="flex w-[95%] items-center relative">
       <input
@@ -8,12 +14,20 @@ const InputFields = () => {
         placeholder="Type what you wanna type"
         className="w-full rounded-full p-5 text-xl border-none transition duration-200 shadow-inner 
         focus:shadow-[0_0_10px_1000px_rgba(0,0,0,0.1)] focus:outline-none"
+        value={todo}
+        onChange={
+          (e)=>setTodo(e.target.value)
+        }
       />
-      <button className="border" type="submit">Go</button>
+      <button
+        className="absolute w-[50px] h-[50px] m-3 rounded-full right-0 border-none 
+        text-sm bg-[#2f74c0] text-white transition duration-200 shadow-[0_0_10px_black]"
+        type="submit"
+      >
+        Go
+      </button>
     </form>
   );
 };
 
-
-
-export default InputFields
+export default InputFields;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
-import Image from "next/image";
+"use client"
+
 import InputFields from "./components/InputFields";
 import "./globals.css";
+import { useState } from "react";
+
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+
+  console.log(todo)
+
   return (
     <div className="w-lvw h-lvh flex flex-col items-center bg-blue-300">
-      <span className="font-serif uppercase text-4xl mt-8 text-black z-10 text-center" >to-do-list</span>
-      <InputFields />
+      <span className="font-serif uppercase text-4xl mt-8 text-black z-10 text-center mb-3" >to-do-list</span>
+      <InputFields todo={todo} setTodo={setTodo}/>
     </div>
   );
 }
